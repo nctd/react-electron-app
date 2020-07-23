7a00be14285453513d61ac6eec821c99c3b2f3b8
Nicolas <nicolastd@outlook.com>
2020-07-23 01:25:59 -0400

    formRecarga

--- src/form.js
+++ src/form.js
@@ -184,6 +184,8 @@ const formServicio = {
 
 let formRevision = [];
 let formMantenimiento = [];
+let formRecarga = [];
+let formPrueba = [];
 
 const wizardMeta = {
   steps: [
@@ -604,6 +606,99 @@ export default () => {
     formMantenimiento = [];
   }
 
+  if (
+    form.getFieldInstance("recarga") &&
+    !form.getFieldInstance("recarga").props["checked"]
+  ) {
+    if (!form.getFieldInstance("fr_recarga1")) {
+      formRecarga.push(
+        {
+          key: "fr_recarga",
+          render() {
+            return (
+              <fieldset>
+                <legend>Recarga</legend>
+              </fieldset>
+            );
+          },
+        },
+        {
+          key: "fr_recarga1",
+          label: "Persona que realizó la recarga",
+          formItemLayout: {
+            labelCol: { span: 9 },
+            wrapperCol: { span: 8 },
+          },
+        },
+        {
+          key: "fr_recarga2",
+          label: "Normas Chilenas aplicadas",
+          formItemLayout: {
+            labelCol: { span: 9 },
+            wrapperCol: { span: 8 },
+          },
+        },
+        {
+          key: "fr_recarga3",
+          label: "Se utilizó el manual del fabricante o armador",
+          widget: "radio-group",
+          // forwardRef: true,
+          options: ["Si", "No"],
+          labelCol: { span: 9 },
+        },
+        {
+          key: "fr_recarga4",
+          label: "Agente de extinción utilizado",
+          formItemLayout: {
+            labelCol: { span: 9 },
+            wrapperCol: { span: 8 },
+          },
+        },
+        {
+          key: "fr_recarga5",
+          label: "Masa extintor antes de la recarga (Kg)",
+          formItemLayout: {
+            labelCol: { span: 9 },
+            wrapperCol: { span: 8 },
+          },
+        },
+        {
+          key: "fr_recarga6",
+          label: "Masa extintor después de la recarga (Kg)",
+          formItemLayout: {
+            labelCol: { span: 9 },
+            wrapperCol: { span: 8 },
+          },
+        },
+        {
+          key: "fr_recarga7",
+          label: "Verificación de estanqueidad después de la recarga",
+          widget: "radio-group",
+          // forwardRef: true,
+          options: ["Si", "No", "No aplica"],
+          labelCol: { span: 9 },
+        },
+        {
+          key: "fr_recarga9",
+          label: "Comentarios",
+          widget: "textarea",
+          formItemLayout: {
+            labelCol: { span: 9 },
+            wrapperCol: { span: 8 },
+          },
+        }
+      );
+    }
+  }
+
+  if (!form.getFieldValue("recarga")) {
+    formRecarga.forEach((e) => {
+      const key = e.key;
+      testform.setFieldsValue({ [key]: null });
+    });
+    formRecarga = [];
+  }
+
   if (currentStep === 0) {
     return (
       <Form
@@ -675,6 +770,9 @@ export default () => {
           </fieldset>
           <fieldset>
             <FormBuilder form={testform} meta={formMantenimiento} />
+          </fieldset>
+          <fieldset>
+            <FormBuilder form={testform} meta={formRecarga} />
           </fieldset>
         </div>
         <Form.Item className="form-footer" style={{ textAlign: "right" }}>
